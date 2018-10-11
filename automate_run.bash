ac2dcad4938f4eef59c0e3fd789c0006baea1e01
Ana Costa <anacost@users.noreply.github.com>
2018-10-11 15:40:27 +0200

    deleted touch, added comments

--- automate_run.bash
+++ automate_run.bash
@@ -4,16 +4,14 @@ RESULT_DIR=/home/anacos/Documents/data-shell/results   #$PWD/results
 #input directory for molecules data
 INPUT_DIR=/home/anacos/Documents/data-shell/molecules
 #Do not change here except you know what you are doing!
-
+#First remove RESULT_DIR
 rm -rf $RESULT_DIR
+#create directory:
 mkdir $RESULT_DIR
-touch $RESULT_DIR/length.txt
+
 for filename in $INPUT_DIR/*.pdb
-
     do
-
    wc -l $filename | sort -n | head -n 1 >> $RESULT_DIR/lengths.txt
-
 done
 
 cat $RESULT_DIR/lengths.txt
